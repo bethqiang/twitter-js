c9d7d26c60f9769f639896364ca31098f72f81f7
Beth Qiang <bethany.qiang@gmail.com>
2016-11-07 17:06:02 -0600

    Create logging middleware

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const volleyball = require('volleyball');
 
 app.listen(port, function() {
   console.log("server listening");
 })
 
+app.use(volleyball);
+
+app.use('/', function(req, res, next) {
+  console.log(req.method, req.path);
+  next();
+})
+
+app.use('/special/', function(req, res, next) {
+  console.log("you reached the special area");
+  next();
+})
+
 app.get('/', function(req, res) {
   res.send("Hello World!");
 })
+
+// app.get('/special/')
