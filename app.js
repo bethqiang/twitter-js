f43066fcd34bd10f71ae7c3167e01efa1a2433f0
Beth Qiang <bethany.qiang@gmail.com>
2016-11-08 08:19:52 -0600

    Add dummy template

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const volleyball = require('volleyball');
+// not necessary right now--we made our own logging middleware
+// const volleyball = require('volleyball');
+const nunjucks = require('nunjucks');
 
 app.listen(port, function() {
   console.log("server listening");
 })
 
-app.use(volleyball);
+// app.use(volleyball);
 
 app.use('/', function(req, res, next) {
   console.log(req.method, req.path);
@@ -23,4 +25,22 @@ app.get('/', function(req, res) {
   res.send("Hello World!");
 })
 
-// app.get('/special/')
+const locals = {
+  people: [
+  {name: 'Gandalf'},
+  {name: 'Frodo'},
+  {name: 'Hermione'}
+]}
+
+// const people = {
+//   name: 'Gandalf',
+//   name: 'Frodo',
+//   name: 'Hermione'
+// } => why not this???
+
+nunjucks.configure('views')
+nunjucks.render('index.html', locals, function(err, output) {
+  if (err) throw err;
+  console.log(output);
+})
+// i'm confused by basically all of this
