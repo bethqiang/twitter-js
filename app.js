d605668e892e1454b909cf97872796eea32c118c
cumins <cuminsjoseph@gmail.com>
2016-11-08 12:58:16 -0500

    up until style stuff

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const port = 3000;
 // not necessary right now--we made our own logging middleware
 // const volleyball = require('volleyball');
 const nunjucks = require('nunjucks');
+const routes = require('./routes/');
 
 app.listen(port, function() {
   console.log("server listening");
@@ -16,31 +17,36 @@ app.use('/', function(req, res, next) {
   next();
 })
 
-app.use('/special/', function(req, res, next) {
-  console.log("you reached the special area");
-  next();
-})
+// app.use('/special/', function(req, res, next) {
+//   console.log("you reached the special area");
+//   next();
+// })
 
 app.get('/', function(req, res) {
   res.send("Hello World!");
 })
 
-const locals = {
-  people: [
-  {name: 'Gandalf'},
-  {name: 'Frodo'},
-  {name: 'Hermione'}
-]}
+// const locals = {
+//   people: [
+//   {name: 'Gandalf'},
+//   {name: 'Frodo'},
+//   {name: 'Hermione'}
+// ]}
 
-// const people = {
-//   name: 'Gandalf',
-//   name: 'Frodo',
-//   name: 'Hermione'
-// } => why not this???
+app.set('view engine', 'html'); // have res.render work with html files
+app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
 
-nunjucks.configure('views')
-nunjucks.render('index.html', locals, function(err, output) {
-  if (err) throw err;
-  console.log(output);
-})
-// i'm confused by basically all of this
+nunjucks.configure('views', { noCache: true });
+//nunjucks.render('index.html', locals, function(err, output) {
+//  if (err) throw err;
+//  console.log(output);
+//})
+
+
+// app.get('/people/', function(req, res){
+//   res.render( 'index', {people: locals.people}, function(err, html) {
+//     res.send(html);
+// });
+// });
+
+app.use('/', routes);
